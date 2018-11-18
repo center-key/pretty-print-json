3167f97a902cecc3eba4be4625ffde4e40d5d24d
Dem Pilafian <dem@centerkey.com>
2018-11-18 02:28:23 -0800

    Release v0.0.1

--- README.md
+++ README.md
@@ -4,6 +4,26 @@
 _JavaScript library to pretty-print JSON data to HTML for formatted color display_
 
 [![License:MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/center-key/pretty-print-json/blob/master/LICENSE.txt)
+[![npm](https://img.shields.io/npm/v/pretty-print-json.svg)](https://www.npmjs.com/package/pretty-print-json)
+[![Dependencies](https://david-dm.org/center-key/pretty-print-json/status.svg)](https://david-dm.org/center-key/pretty-print-json)
+[![Vulnerabilities](https://snyk.io/test/github/center-key/pretty-print-json/badge.svg)](https://snyk.io/test/github/center-key/pretty-print-json)
+[![Build](https://travis-ci.org/center-key/pretty-print-json.svg)](https://travis-ci.org/center-key/pretty-print-json)
+
+## Setup
+From the [jsdelivr.com CDN](https://www.jsdelivr.com/package/npm/pretty-print-json):
+```html
+<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/pretty-print-json@0.0/pretty-print-json.css>
+...
+<script src=https://cdn.jsdelivr.net/npm/pretty-print-json@0.0/pretty-print-json.min.js></script>
+```
+
+In a Node.js project:
+```shell
+$ npm install pretty-print-json
+```
+```javascript
+const prettyPrintJson = require('pretty-print-json');
+```
 
 ## Usage
 ```javascript
---
+++ dist/pretty-print-json.js
@@ -0,0 +1,32 @@
+//! pretty-print-json v0.0.1 ~ github.com/center-key/pretty-print-json ~ MIT License
+const prettyPrintJson = {
+   version: '0.0.1',
+   toHtml: function(obj) {
+      function replacer(match, pIndent, pKey, pVal, pEnd) {
+         const key =  '<span class=json-key>';
+         const val =  '<span class=json-value>';
+         const bool = '<span class=json-boolean>';
+         const str =  '<span class=json-string>';
+         const isBool = ['true', 'false'].includes(pVal);
+         const pValSpan = /^"/.test(pVal) ? str : isBool ? bool : val;
+         let r = pIndent || '';
+         if (pKey)
+            r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+         if (pVal)
+            r = r + pValSpan + pVal + '</span>';
+         return r + (pEnd || '');
+         }
+      const jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+      return JSON.stringify(obj, null, 3)
+         .replace(/&/g, '&amp;')
+         .replace(/\\"/g, '&quot;')
+         .replace(/</g, '&lt;')
+         .replace(/>/g, '&gt;')
+         .replace(jsonLine, replacer);
+      }
+   };
+
+if (typeof module === 'object')
+   module.exports = prettyPrintJson;  //Node.js module loading system (CommonJS)
+if (typeof window === 'object')
+   window.prettyPrintJson = prettyPrintJson;  //support both global and window property
