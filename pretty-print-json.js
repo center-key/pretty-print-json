ee1e92be245e0686b9d80cd03c13af7e1fb7e0e3
Dem Pilafian <dem@centerkey.com>
2018-11-19 01:19:09 -0800

    Next release

--- pretty-print-json.js
+++ pretty-print-json.js
@@ -1,5 +1,5 @@
 const prettyPrintJson = {
-   version: '0.0.2',
+   version: '0.0.3',
    toHtml: function(obj) {
       function replacer(match, pIndent, pKey, pVal, pEnd) {
          const key =  '<span class=json-key>';
