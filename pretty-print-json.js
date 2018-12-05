f5311534bfd3b3182e1692089c7a0c10a484e981
Dem Pilafian <dem@centerkey.com>
2018-12-05 02:50:09 -0800

    Upgrade to es6 functions

--- pretty-print-json.js
+++ pretty-print-json.js
@@ -5,30 +5,30 @@ const prettyPrintJson = {
    version: '[VERSION]',
 
    toHtml: function(thing) {
-      function makeHtmlFriendly(string) {
+      const makeHtmlFriendly = (string) => {
          // Converts characters into their HTML entities
          return string
             .replace(/&/g,   '&amp;')
             .replace(/\\"/g, '&quot;')
             .replace(/</g,   '&lt;')
             .replace(/>/g,   '&gt;');
-         }
-      function replacer(match, p1, p2, p3, p4) {
+         };
+      const replacer = (match, p1, p2, p3, p4) => {
          // Converts the four parenthesized capture groups (indent, key, value, end) into HTML
-         const part = { indent: p1, key: p2, value: p3, end: p4 };
-         const key =  '<span class=json-key>';
-         const val =  '<span class=json-value>';
-         const bool = '<span class=json-boolean>';
-         const str =  '<span class=json-string>';
-         const isBool = ['true', 'false'].includes(part.value);
-         const valSpan = /^"/.test(part.value) ? str : isBool ? bool : val;
-         const findName = /"([\w]+)": |(.*): /;
+         const part =       { indent: p1, key: p2, value: p3, end: p4 };
+         const key =        '<span class=json-key>';
+         const val =        '<span class=json-value>';
+         const bool =       '<span class=json-boolean>';
+         const str =        '<span class=json-string>';
+         const isBool =     ['true', 'false'].includes(part.value);
+         const valSpan =    /^"/.test(part.value) ? str : isBool ? bool : val;
+         const findName =   /"([\w]+)": |(.*): /;
          const indentHtml = part.indent || '';
          const keyHtml =    part.key ? key + part.key.replace(findName, '$1$2') + '</span>: ' : '';
          const valueHtml =  part.value ? valSpan + part.value + '</span>' : '';
          const endHtml =    part.end || '';
          return indentHtml + keyHtml + valueHtml + endHtml;
-         }
+         };
       const jsonLine = /^( *)("[^"]+": )?("[^"]*"|[\w.+-]*)?([{}[\],]*)?$/mg;
       // Regex parses each line of the JSON string into four parts:
       //    Capture group       Part    Description                  '   "active": true,'
