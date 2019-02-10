9c52326699f49a7c9f4f45014caa7205e6b75578
Dem Pilafian <dem@centerkey.com>
2019-02-10 01:32:51 -0800

    Better function name

--- pretty-print-json.js
+++ pretty-print-json.js
@@ -5,8 +5,8 @@ const prettyPrintJson = {
    version: '[VERSION]',
 
    toHtml: (thing) => {
-      const makeHtmlFriendly = (string) => {
-         // Converts characters into their HTML entities
+      const htmlEntities = (string) => {
+         // Makes text displayable in browsers
          return string
             .replace(/&/g,   '&amp;')
             .replace(/\\"/g, '&quot;')
@@ -31,13 +31,13 @@ const prettyPrintJson = {
          };
       const jsonLine = /^( *)("[^"]+": )?("[^"]*"|[\w.+-]*)?([{}[\],]*)?$/mg;
       // Regex parses each line of the JSON string into four parts:
-      //    Capture group       Part    Description                  '   "active": true,'
-      //    ------------------  ------  ---------------------------  --------------------
-      //    ( *)                indent  Spaces for indentation       '   '
-      //    ("[^"]+": )         key     Key name                     '"active": '
-      //    ("[^"]*"|[\w.+-]*)  value   Key value                    'true'
-      //    ([{}[\],]*)         end     Line termination characters  ','
-      return makeHtmlFriendly(JSON.stringify(thing, null, 3)).replace(jsonLine, replacer);
+      //    Capture group       Part        Description                  '   "active": true,'
+      //    ------------------  ----------  ---------------------------  --------------------
+      //    ( *)                p1: indent  Spaces for indentation       '   '
+      //    ("[^"]+": )         p2: key     Key name                     '"active": '
+      //    ("[^"]*"|[\w.+-]*)  p3: value   Key value                    'true'
+      //    ([{}[\],]*)         p4: end     Line termination characters  ','
+      return htmlEntities(JSON.stringify(thing, null, 3)).replace(jsonLine, replacer);
       }
 
    };
