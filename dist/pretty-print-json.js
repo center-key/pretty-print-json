61822fa2fbf59de528e705552cd59a2eb3360d35
Dem Pilafian <dem@centerkey.com>
2020-03-15 00:21:44 -0700

    Release v0.2.1

--- dist/pretty-print-json.js
+++ dist/pretty-print-json.js
@@ -1,8 +1,8 @@
-//! pretty-print-json v0.2.0 ~ github.com/center-key/pretty-print-json ~ MIT License
+//! pretty-print-json v0.2.1 ~ github.com/center-key/pretty-print-json ~ MIT License
 
 const prettyPrintJson = {
 
-   version: '0.2.0',
+   version: '0.2.1',
 
    toHtml(thing, options) {
       const defaults = { indent: 3, quoteKeys: false };
