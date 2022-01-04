e5a3854e95e402203cd3a5d8d6794bb9e2ea098b
dpilafian <dem@centerkey.com>
2022-01-03 16:38:12 -0800

    More helpful code comments

--- pretty-print-json.ts
+++ pretty-print-json.ts
@@ -14,18 +14,17 @@ const prettyPrintJson = {
    toHtml(thing: unknown, options?: FormatOptions): string {
       const defaults = { indent: 3, linkUrls: true, quoteKeys: false };
       const settings = { ...defaults, ...options };
-      const htmlEntities = (text: string) => {
-         // Makes text displayable in browsers
-         return text
-            .replace(/&/g,   '&amp;')
-            .replace(/\\"/g, '&bsol;&quot;')
-            .replace(/</g,   '&lt;')
-            .replace(/>/g,   '&gt;');
-         };
+      const htmlEntities = (text: string) => text
+         // Makes text displayable in browsers.
+         .replace(/&/g,   '&amp;')
+         .replace(/\\"/g, '&bsol;&quot;')
+         .replace(/</g,   '&lt;')
+         .replace(/>/g,   '&gt;');
       const spanTag = (type: JsonType, display?: string): string =>
+         // Creates HTML to display a value like: like "<span class=json-boolean>true</span>"
          display ? '<span class=json-' + type + '>' + display + '</span>' : '';
       const buildValueHtml = (value: string): string => {
-         // Returns a string like: "<span class=json-number>3.1415</span>"
+         // Analyzes a value and returns HTML like: "<span class=json-number>3.1415</span>"
          const strType =  /^"/.test(value) && 'string';
          const boolType = ['true', 'false'].includes(value) && 'boolean';
          const nullType = value === 'null' && 'null';
@@ -36,7 +35,7 @@ const prettyPrintJson = {
          return spanTag(type, display);
          };
       const replacer = (match: string, p1: string, p2: string, p3: string, p4: string): string => {
-         // Converts the four parenthesized capture groups (indent, key, value, end) into HTML
+         // Converts the four parenthesized capture groups (indent, key, value, end) into HTML.
          const part =       { indent: p1, key: p2, value: p3, end: p4 };
          const findName =   settings.quoteKeys ? /(.*)(): / : /"([\w$]+)": |(.*): /;
          const indentHtml = part.indent || '';
@@ -48,12 +47,13 @@ const prettyPrintJson = {
          };
       const jsonLine = /^( *)("[^"]+": )?("[^"]*"|[\w.+-]*)?([{}[\],]*)?$/mg;
          // Regex parses each line of the JSON string into four parts:
-         //    Capture group       Part        Description                  '   "active": true,'
+         //    Capture group       Part        Description                  Example
          //    ------------------  ----------  ---------------------------  --------------------
          //    ( *)                p1: indent  Spaces for indentation       '   '
          //    ("[^"]+": )         p2: key     Key name                     '"active": '
          //    ("[^"]*"|[\w.+-]*)  p3: value   Key value                    'true'
          //    ([{}[\],]*)         p4: end     Line termination characters  ','
+         // For example, '   "active": true,' is parsed into: ['   ', '"active": ', 'true', ',']
       const json = JSON.stringify(thing, null, settings.indent) || 'undefined';
       return htmlEntities(json).replace(jsonLine, replacer);
       },
