f15fd4e28afe7cfb81c3837bc06dfbd81842cbd0
dpilafian <dem@centerkey.com>
2022-03-06 19:04:35 -0800

    Test case to verify dist folder files

---
+++ spec/package.spec.js
@@ -0,0 +1,25 @@
+// Pretty Print JSON
+// Mocha Specification Suite
+
+// Imports
+import { assertDeepStrictEqual } from 'assert-deep-strict-equal';
+import { readdirSync } from 'fs';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('The "dist" folder', () => {
+
+   it('contains the correct files', () => {
+      const actual = readdirSync('dist').sort();
+      const expected = [
+         'pretty-print-json.css',
+         'pretty-print-json.d.ts',
+         'pretty-print-json.dark-mode.css',
+         'pretty-print-json.dev.js',
+         'pretty-print-json.js',
+         'pretty-print-json.min.js',
+         'pretty-print-json.umd.cjs',
+         ];
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
