a4639756e23bbc6d7c9745a9e01a4649e835cc49
dpilafian <dem@centerkey.com>
2022-10-02 19:17:47 -0700

    Cleaner build steps with uglify-js

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@
 // gulp configuration and tasks
 
 // Imports
-import babel       from 'gulp-babel';
-import gap         from 'gulp-append-prepend';
 import gulp        from 'gulp';
 import mergeStream from 'merge-stream';
 import rename      from 'gulp-rename';
@@ -12,27 +10,11 @@ import size        from 'gulp-size';
 import { readFileSync } from 'fs';
 
 // Setup
-const pkg =           JSON.parse(readFileSync('package.json', 'utf-8'));
-const minorVersion =  pkg.version.split('.').slice(0, 2).join('.');
-const transpileES6 =  ['@babel/env', { modules: false }];
-const babelMinifyJs = { presets: [transpileES6, 'minify'], comments: false };
+const pkg =          JSON.parse(readFileSync('package.json', 'utf-8'));
+const minorVersion = pkg.version.split('.').slice(0, 2).join('.');
 
 // Tasks
 const task = {
-
-   minifyJs() {
-      return gulp.src('build/pretty-print-json.js')
-         .pipe(replace(/^export { (.*) };/m, 'if (typeof window === "object") window.$1 = $1;'))
-         .pipe(rename({ extname: '.dev.js' }))
-         .pipe(size({ showFiles: true }))
-         .pipe(gulp.dest('build'))
-         .pipe(babel(babelMinifyJs))
-         .pipe(rename('pretty-print-json.min.js'))
-         .pipe(gap.appendText('\n'))
-         .pipe(size({ showFiles: true }))
-         .pipe(size({ showFiles: true, gzip: true }))
-         .pipe(gulp.dest('build'));
-      },
 
    publishWebsite() {
       const cdnUri = 'https://cdn.jsdelivr.net/npm/pretty-print-json@' + minorVersion;
@@ -57,5 +39,4 @@ const task = {
    };
 
 // Gulp
-gulp.task('minify-js',       task.minifyJs);
 gulp.task('publish-website', task.publishWebsite);
