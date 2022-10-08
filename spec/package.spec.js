2a0a79b642aecb0f527ee092ae97fbb4366d8161
dpilafian <dem@centerkey.com>
2022-10-07 17:42:45 -0700

    Add minified versions of the css files

--- spec/package.spec.js
+++ spec/package.spec.js
@@ -14,8 +14,10 @@ describe('The "dist" folder', () => {
          'pretty-print-json.css',
          'pretty-print-json.d.ts',
          'pretty-print-json.dark-mode.css',
+         'pretty-print-json.dark-mode.min.css',
          'pretty-print-json.dev.js',
          'pretty-print-json.js',
+         'pretty-print-json.min.css',
          'pretty-print-json.min.js',
          'pretty-print-json.umd.cjs',
          ];
