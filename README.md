f137ee31861a489bce1eca012d880ce160016db9
dpilafian <dem@centerkey.com>
2023-01-07 14:16:32 -0800

    Seperate dist folder for all css files

--- README.md
+++ README.md
@@ -21,12 +21,13 @@ https://pretty-print-json.js.org
 ### 1. Web browser
 Load from the [jsdelivr.com CDN](https://www.jsdelivr.com/package/npm/pretty-print-json):
 ```html
-<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/pretty-print-json@1.3/dist/pretty-print-json.css>
+<link rel=stylesheet href=https://cdn.jsdelivr.net/npm/pretty-print-json@1.3/dist/css/pretty-print-json.css>
 ...
 <script src=https://cdn.jsdelivr.net/npm/pretty-print-json@1.3/dist/pretty-print-json.min.js></script>
 ```
 For **dark mode**, replace `pretty-print-json.css` with `pretty-print-json.dark-mode.css` in
 the `<link>` tag.
+Or to automatically sense **dark mode** using the `prefers-color-scheme` CSS media feature, use `pretty-print-json.prefers-color.css` instead.
 ### 2. Node.js server
 Install package for node:
 ```shell
--- spec/package.spec.js
+++ spec/package.spec.js
@@ -11,15 +11,23 @@ describe('The "dist" folder', () => {
    it('contains the correct files', () => {
       const actual = fs.readdirSync('dist').sort();
       const expected = [
-         'pretty-print-json.css',
+         'css',
          'pretty-print-json.d.ts',
-         'pretty-print-json.dark-mode.css',
-         'pretty-print-json.dark-mode.min.css',
          'pretty-print-json.dev.js',
          'pretty-print-json.js',
-         'pretty-print-json.min.css',
          'pretty-print-json.min.js',
          'pretty-print-json.umd.cjs',
+         ];
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   it('contains a "css" subfolder with the correct files', () => {
+      const actual = fs.readdirSync('dist/css').sort();
+      const expected = [
+         'pretty-print-json.css',
+         'pretty-print-json.dark-mode.css',
+         'pretty-print-json.dark-mode.min.css',
+         'pretty-print-json.min.css',
          ];
       assertDeepStrictEqual(actual, expected);
       });
